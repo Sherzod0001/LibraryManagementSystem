ae7151f193546b07a3bf91389e4a1d347ed5ac82
The-Leon <user@WIN-A6H3AK64JSB>
2024-07-05 03:09:14 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Library library = new Library();
+
+        string answer;
+        do
+        {
+            Console.WriteLine("My personal library : ");
+            Console.WriteLine("1. list of books");
+            Console.WriteLine("2. Add a book");
+            Console.WriteLine("3. Remove Book");
+            Console.WriteLine("4. Exit");
+            Console.Write("Enter your choice: ");
+
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    library.DisplayBooks();
+                    break;
+                case "2":
+                    Console.Write("Enter the title of the book to add: ");
+                    string bookTitleToAdd = Console.ReadLine();
+                    Console.Write("Enter the author of the book: ");
+                    string bookAuthorToAdd = Console.ReadLine();
+                    library.AddBook(bookTitleToAdd, bookAuthorToAdd);
+                    break;
+                case "3":
+                    library.DisplayBooks();
+                    Console.Write("Enter the index of the book to remove: ");
+                    if (int.TryParse(Console.ReadLine(), out int index))
+                    {
+                        library.RemoveBook(index - 1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid index, please enter a valid number.");
+                    }
+                    break;
+                case "4":
+                    Console.WriteLine("Exiting...");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice, please try again.");
+                    break;
+            }
+               
+            
+            Console.Write("Do you want to continue? (yes/y to continue any other key to exit): ");
+            answer = Console.ReadLine();
+
+        } while (answer == "yes" || answer == "y");
+    }
+}
---
+++ Services/Library.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Library
+{
+    private List<Book> books = new List<Book>();
+
+    public void DisplayBooks()
+    {
+        Console.WriteLine("\nCurrent books in the library:");
+        for (int i = 0; i < books.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {books[i].Title} by {books[i].Author}");
+        }
+    }
+
+    public void AddBook(string title, string author)
+    {
+        books.Add(new Book { Title = title, Author = author });
+        Console.WriteLine($"Book '{title}' by {author} added to the library.");
+    }
+
+    public void RemoveBook(int index)
+    {
+        if (index >= 0 && index < books.Count)
+        {
+            string removedBookTitle = books[index].Title;
+            books.RemoveAt(index);
+            Console.WriteLine($"Book '{removedBookTitle}' removed from the library.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid index, please try again.");
+        }
+    }
+}
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+}
